d84bc1d978a262c6693c30014f87554d60acc547
Adrian Hordyk <ar.hordyk@gmail.com>
2022-07-27 14:51:55 -0700

    Create generate_OMs.R

---
+++ generate_OMs.R
@@ -0,0 +1,20 @@
+# devtools::install_github("nikolaifish/bamExtras")
+library(bamExtras)
+library(MSEtool)
+
+fls <- list.files('functions')
+for (f in fls) source(file.path('functions', f))
+
+
+# Red snapper OM and MOM
+rs <- standardize_rdat(rdat_RedSnapper)
+OM <- BAM2OM(rs, nsim = 3)
+MOM <- BAM2MOM(rs, nsim = 3)
+
+Hist <- Simulate(OM)
+
+# Gag OM and MOM
+gag <- standardize_rdat(rdat_GagGrouper)
+OM <- BAM2OM(gag, nsim = 3)
+MOM <- BAM2MOM(gag, nsim = 3)
+
