490cc206416aa82ff0f46c351347efc39f7dc0ac
Adrian <ar.hordyk@gmail.com>
2024-02-07 15:58:44 -0800

    Update server.R

--- inst/shiny_app/server.R
+++ inst/shiny_app/server.R
@@ -72,16 +72,16 @@ OM_ReconstructServer <- function(id='reconstruct', number) {
       return(out)
     })
 
-    output$SSBhist <- renderPlot({
-      SSBhist <- MSE_info[[input$reconstruct_OM]]$Historical$SSB
-      Ref_Points <- MSE_info[[input$reconstruct_OM]]$Ref_Points
-      rel_to <- input$rel_to
-      if(rel_to=='None') {
-        rel_to <- NA
-      }
-
-      plot_SB_hist(SSBhist, Ref_Points, rel_to=rel_to)
-    })
+    # output$SSBhist <- renderPlot({
+    #   SSBhist <- MSE_info[[input$reconstruct_OM]]$Historical$SSB
+    #   Ref_Points <- MSE_info[[input$reconstruct_OM]]$Ref_Points
+    #   rel_to <- input$rel_to
+    #   if(rel_to=='None') {
+    #     rel_to <- NA
+    #   }
+    #
+    #   plot_SB_hist(SSBhist, Ref_Points, rel_to=rel_to)
+    # })
 
 
   })
