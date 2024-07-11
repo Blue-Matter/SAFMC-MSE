62968c33dfc03211e222955039b5e68e236aa2e0
Adrian <ar.hordyk@gmail.com>
2024-07-11 11:19:10 -0700

    update vignette

--- Run_Analysis/7. Results_Base_Case.R
+++ Run_Analysis/7. Results_Base_Case.R
@@ -1,138 +1,159 @@
 
+MP_levels <- function() {
+  c('SQ', 'SQ_FR', 'SQ_MLL', 'SQ_FR_MLL',
+    'SQ_NS', 'SQ_OS', 'SQ_MLL_NS', 'SQ_MLL_OS',
+    'SQ_FR_NS', 'SQ_FR_OS',
+    'SQ_FR_MLL_NS', 'SQ_FR_MLL_OS')
+}
 
-make_DFs <- function(stock='Red snapper', om='BaseCase', MSE_dir='MSE_objects') {
+Stock_levels <- function() {
+  c('Red snapper', 'Gag grouper', 'Black sea bass')
+}
 
-  stock_code <- switch(stock,
-                       "Red snapper"='RS',
-                       "Gag grouper" = "GG",
-                       "Black sea bass" ='BS'
-  )
+make_DFs <- function(om='BaseCase', MSE_dir='MSE_Objects') {
 
   MSE_files <- list.files(MSE_dir)
-  stock_mse_files <- MSE_files[grepl(paste(om, stock_code, sep='_'), MSE_files)]
-  MP_text <- lapply(strsplit(stock_mse_files, paste0(paste(om, stock_code, sep='_'), '_')), '[[', 2) |> unlist()
-  MP_names <- unlist(strsplit(MP_text, '.mmse'))
+  OM_fls <- lapply(strsplit(MSE_files, paste0(om, '_')), '[[', 2) |> unlist()
+  stocks <- lapply(strsplit(OM_fls, '_'), '[[', 1) |> unlist() |> unique()
 
-  Ref_DF <- readRDS('Misc_Objects/ref_df.rda')
-  Ref_DF$OM <- gsub(' ', '',Ref_DF$OM)
-  Ref_DF <- Ref_DF|> dplyr::filter(Stock==stock, OM==om)
+  st_list <- list()
+  for (st in seq_along(stocks)) {
+    stock_code <- stocks[st]
+    stock <- switch(stock_code,
+                    'RS'="Red snapper",
+                    "GG"="Gag grouper",
+                    'BS'="Black sea bass"
+    )
+    MSEtool:::message_info('Processing', stock, paste0(st, '/', length(stocks)))
 
-  for (i in seq_along(stock_mse_files)) {
-    MSE <- readRDS(file.path(MSE_dir, stock_mse_files[i]))
-    project_yrs <- seq(MSE@multiHist[[1]][[1]]@OMPars$CurrentYr[1]+1, by=1, length.out=MSE@proyears)
+    stock_mse_files <- MSE_files[grepl(paste(om, stock_code, sep='_'), MSE_files)]
+    MP_text <- lapply(strsplit(stock_mse_files, paste0(paste(om, stock_code, sep='_'), '_')), '[[', 2) |> unlist()
+    MP_names <- unlist(strsplit(MP_text, '.mmse'))
 
-    MP_name <- MP_names[i]
-    rec_reduction <- lapply(strsplit(MSE@MPs[[1]], paste0(MP_name,'_')), '[[', 2) |> unlist() |> as.numeric()
+    Ref_DF <- readRDS('Misc_Objects/ref_df.rda')
+    Ref_DF$OM <- gsub(' ', '',Ref_DF$OM)
+    Ref_DF <- Ref_DF|> dplyr::filter(Stock==stock, OM==om)
 
-    mps <- MSE@MPs[[1]]
-    nMPs <- length(mps)
+    mp_list <- list()
+    for (i in seq_along(stock_mse_files)) {
+      MSEtool:::message_info(stock_mse_files[i], paste0(i, '/', length(stock_mse_files)))
+      MSE <- readRDS(file.path(MSE_dir, stock_mse_files[i]))
+      project_yrs <- seq(MSE@multiHist[[1]][[1]]@OMPars$CurrentYr[1]+1, by=1, length.out=MSE@proyears)
 
-    # SSB
-    df <- data.frame(OM=om,
-                     Stock=stock,
-                     Sim=1:MSE@nsim,
-                     Year=rep(project_yrs, each=MSE@nsim*nMPs),
-                     MP=rep(mps, each=MSE@nsim),
-                     SSB=as.vector(MSE@SSB[,1,,]),
-                     MSST=Ref_DF$MSST)
+      MP_name <- MP_names[i]
+      rec_reduction <- lapply(strsplit(MSE@MPs[[1]], paste0(MP_name,'_')), '[[', 2) |> unlist() |> as.numeric()
 
-    df$MP_Name <- MP_name
-    df$Rec_Reduction <- rep(rec_reduction, each=MSE@nsim)
+      mps <- MSE@MPs[[1]]
+      nMPs <- length(mps)
 
-    nm <- paste0(paste(om, stock_code, MP_name, sep='_'), '.rda')
+      # SSB
+      df <- data.frame(OM=om,
+                       Stock=stock,
+                       Sim=1:MSE@nsim,
+                       Year=rep(project_yrs, each=MSE@nsim*nMPs),
+                       MP=rep(mps, each=MSE@nsim),
+                       SSB=as.vector(MSE@SSB[,1,,]),
+                       MSST=Ref_DF$MSST)
 
-    saveRDS(df, file.path('Results_Objects/SSB', nm))
+      df$MP_Name <- MP_name
+      df$Rec_Reduction <- rep(rec_reduction, each=MSE@nsim)
 
-    # Apical F,  Landings and Discards
-    nfleet <- length(MSE@Fleets[[1]])
-    fleet_names <- MSE@Fnames[,1]
+      nm <- paste0(paste(om, stock_code, MP_name, sep='_'), '.rda')
 
-    df <- data.frame(OM=om,
-                     Stock=stock,
-                     Sim=1:MSE@nsim,
-                     Fleet=rep(fleet_names, each=MSE@nsim),
-                     MP=rep(mps, each=MSE@nsim*nfleet),
-                     Year=rep(project_yrs, each=MSE@nsim*nMPs*nfleet),
-                     F=as.vector(MSE@FM[,1,,,]),
-                     Landings=as.vector(MSE@Catch[,1,,,]),
-                     Discards=as.vector(MSE@Removals[,1,,,] -MSE@Catch[,1,,,]),
-                     MFMT=Ref_DF$MFMT)
+      saveRDS(df, file.path('Results_Objects/SSB', nm))
 
-    df$MP_Name <- MP_name
-    df$Rec_Reduction <- rep(rec_reduction, each=MSE@nsim* nfleet)
+      # Apical F,  Landings and Discards
+      nfleet <- length(MSE@Fleets[[1]])
+      fleet_names <- MSE@Fnames[,1]
 
-    nm <- paste0(paste(om, stock_code, MP_name, sep='_'), '.rda')
+      df <- data.frame(OM=om,
+                       Stock=stock,
+                       Sim=1:MSE@nsim,
+                       Fleet=rep(fleet_names, each=MSE@nsim),
+                       MP=rep(mps, each=MSE@nsim*nfleet),
+                       Year=rep(project_yrs, each=MSE@nsim*nMPs*nfleet),
+                       F=as.vector(MSE@FM[,1,,,]),
+                       Landings=as.vector(MSE@Catch[,1,,,]),
+                       Discards=as.vector(MSE@Removals[,1,,,] -MSE@Catch[,1,,,]),
+                       MFMT=Ref_DF$MFMT)
 
-    saveRDS(df, file.path('Results_Objects/F_Landing_Discards', nm))
-
-
-    # # Mean Age
-    # MSE@N |> dim()
-    # MSE@
-    # MSE@PPD[[1]][[1]][[mm]]@CAA[1,1,]
-    # MSE@PPD[[1]][[1]][[mm]]@CAL[1,1,]
-    #
-    #
-    # # Prob Trophy Fish
-    #
-    #
-    # yr <- 2032
-    # yr_ind <- match(yr, project_yrs)
-    # sim <- 50
-    # mm <- 1
-    # mp <- mps[mm]
-    #
-    # MSE@FM[sim,1,,mm,yr_ind]
-    # df |> dplyr::filter(Sim==sim, Year==yr, MP==mp)
-    #
-    # MSE@Catch[1,1,3,1,]
-    # MSE@Catch[1,1,3,6,]
-    #
+      df$MP_Name <- MP_name
+      df$Rec_Reduction <- rep(rec_reduction, each=MSE@nsim* nfleet)
+      saveRDS(df, file.path('Results_Objects/F_Landing_Discards', nm))
 
 
-
-
-  }
-
+    } # end MP loop
+  } # end stock loop
 
 }
 
-make_DFs()
+make_DFs(om='BaseCase')
 
-
-Plot_Prob_Rebuild <- function(stock='Red snapper', om='BaseCase', rebuild_target=2044) {
-
-  stock_code <- switch(stock,
-                       "Red snapper"='RS',
-                       "Gag grouper" = "GG",
-                       "Black sea bass" ='BS'
-  )
-
-  fls <- list.files("Results_Objects/SSB")
-  fls <- fls[grepl(paste0(paste(om, stock_code, sep='_'), "_"), fls)]
-
-  df_list <- list()
-  for (i in seq_along(fls)) {
-    df_list[[i]] <- readRDS(file.path("Results_Objects/SSB", fls[i]))
+get_rebuild_year <- function(stock_code) {
+  if (stock_code== 'RS') {
+    return(2044)
+  } else if (stock_code== 'GG') {
+    return(2032)
+  } else if (stock_code== 'BS') {
+    return(2032)
+  } else {
+    stop('No rebuilding target specified for ', stock_code)
   }
-  df <- do.call('rbind', df_list) |> dplyr::filter(Year==rebuild_target)
-  df$Rebuilt <- df$SSB>df$MSST
 
-  df2 <- df |> dplyr::group_by(MP_Name, Rec_Reduction) |>
+}
+
+Plot_Prob_Rebuild <- function(om='BaseCase', results_dir='Results_Objects') {
+
+  Results_files <- list.files(file.path(results_dir, 'SSB'))
+
+  OM_fls <- lapply(strsplit(Results_files, paste0(om, '_')), '[[', 2) |> unlist()
+  stocks <- lapply(strsplit(OM_fls, '_'), '[[', 1) |> unlist() |> unique()
+
+  st_list <- list()
+  for (st in seq_along(stocks)) {
+    stock_code <- stocks[st]
+    stock <- switch(stock_code,
+                    'RS'="Red snapper",
+                    "GG"="Gag grouper",
+                    'BS'="Black sea bass"
+    )
+
+    fls <- Results_files[grepl(paste0(paste(om, stock_code, sep='_'), "_"), Results_files)]
+
+    df_list <- list()
+    for (i in seq_along(fls)) {
+      df_list[[i]] <- readRDS(file.path(results_dir, 'SSB', fls[i]))
+    }
+    df <- do.call('rbind', df_list) |> dplyr::filter(Year==get_rebuild_year(stock_code))
+    df$Rebuilt <- df$SSB>df$MSST
+    st_list[[st]] <- df
+  }
+  DF <- do.call('rbind', st_list)
+
+  DF <- DF |> dplyr::group_by(Stock, MP_Name, Rec_Reduction) |>
     dplyr::summarise(Prob=mean(Rebuilt), .groups='drop')
 
-  df2$Rec_Reduction <- factor(df2$Rec_Reduction)
-  df2$MP_Name <- factor(df2$MP_Name, levels=c('SQ', 'SQ_FR', 'SQ_MLL', 'SQ_NS', 'SQ_OS',
-                                              'SQ_FR_MLL', 'SQ_FR_NS', 'SQ_FR_OS',
-                                              'SQ_MLL_NS', 'SQ_MLL_OS',
-                                              'SQ_FR_MLL_NS', 'SQ_FR_MLL_OS'))
-  ggplot(df2, aes(x=Rec_Reduction, y=MP_Name)) +
+  DF$Rec_Reduction <- 1-DF$Rec_Reduction
+
+  DF$Rec_Reduction <- factor(DF$Rec_Reduction, ordered = TRUE,
+                             levels=unique(DF$Rec_Reduction))
+  DF$MP_Name <- factor(DF$MP_Name, levels=MP_levels())
+  DF$Stock <- factor(DF$Stock, levels=Stock_levels())
+
+  ggplot2::ggplot(DF, aes(x=Rec_Reduction, y=MP_Name)) +
+    facet_grid(~Stock) +
     geom_tile(aes(fill=Prob)) +
     geom_text(aes(label = round(Prob, 2))) +
-    scale_fill_gradient2(low = "red", mid='white', high = "green", midpoint=0.5)
-
+    scale_fill_gradient2(low = "red", mid='white', high = "green", midpoint=0.5) +
+    theme_bw() +
+    scale_x_discrete(expand=c(0,0))+
+    scale_y_discrete(expand=c(0,0)) +
+    labs(x='General Recreational Relative Effort',
+         y='Management Actions',
+         fill='Probability')
 
 }
+
 
 rebuild_target <- 2044
 
@@ -143,9 +164,37 @@ ssb <- MSE@SSB[,1,,]
 prob_rebuild <- apply(ssb>=array(ref_targets$MSST, dim=dim(ssb)), 2:3, mean)
 
 
+# TODO:
+# 1. why does RS MLL have worse SSB performance than SQ??
+
+MSE_1 <- readRDS('MSE_Objects/BaseCase_RS_SQ.mmse')
+MSE_2 <- readRDS('MSE_Objects/BaseCase_RS_SQ_MLL.mmse')
+
+MSE_1@MPs[[1]]
+mm <- 91
+
+MSE_1@SB_SBMSY[,1,mm,] |> mean()
+MSE_2@SB_SBMSY[,1,mm,] |> mean()
+
+sim <- 1
+fl <- 3
+mm <- 1
+MSE_1@Removals[sim,1,fl,mm,] - MSE_1@Catch[sim,1,fl,mm,]
+MSE_2@Removals[sim,1,fl,mm,] - MSE_2@Catch[sim,1,fl,mm,]
+
+data.frame(Year=2020:2044,
+           SQ=MSE_1@SB_SBMSY[sim,1,mm,],
+           SQ_MLL=MSE_2@SB_SBMSY[sim,1,mm,])
 
 
+multiHist <- readRDS('Hist_Objects/BaseCase_RS.hist')
+fl <- tempfile()
+fl
+saveRDS(multiHist, fl)
 
+MSE_2@PPD[[1]][[1]][[mm]]@Misc$StockPars
+
+# 2. why does GG NS perform better than OS at lower effort
 ## SB/SBMSST
 
 ## Relative STY
