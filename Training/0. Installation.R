35e4e7c35b3adaea1b1d2eaa2e236e8145d9d82c
Adrian Hordyk <ar.hordyk@gmail.com>
2025-04-27 15:43:39 -0700

    Create 0. Installation.R

---
+++ Training/0. Installation.R
@@ -0,0 +1,18 @@
+
+# Recent version of R
+https://www.r-project.org/
+
+# Recent version of RStudio
+https://posit.co/downloads/
+
+# RTools for install MSEtool
+https://cran.r-project.org/bin/windows/Rtools/rtools45/rtools.html
+
+# Install `pak`
+install.packages('pak')
+
+# Install `SAMSE` package and dependencies
+pak::pkg_install('blue-matter/SAFMC-MSE')
+
+# Install openMSE
+pak::pkg_install('openMSE')
